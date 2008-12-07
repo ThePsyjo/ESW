f99a857643bcdfbdbdec78d83ee8622c2f2d5413
ThePsyjo <ThePsyjo@googlemail.com>
2008-12-07 23:54:57 +0000

    main window, about widgets, nothing more so far

---
+++ src/main.cpp
@@ -0,0 +1,51 @@
+
+/************************************************************************
+ * main.cpp								*
+ * Copyright (C) 2008  Psyjo						*
+ *									*
+ * This program is free software; you can redistribute it and/or modify	*
+ * it under the terms of the GNU General Public License as published by	*
+ * the Free Software Foundation; either version 3 of the License,	*
+ * or (at your option) any later version.				*
+ *									*
+ * This program is distributed in the hope that it will be useful, but	*
+ * WITHOUT ANY WARRANTY; without even the implied warranty of		*
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.			*
+ * See the GNU General Public License for more details.			*
+ *									*
+ * You should have received a copy of the GNU General Public License	*
+ * along with this program; if not, see <http://www.gnu.org/licenses/>. *
+ ************************************************************************/
+
+#include <QApplication>
+#include <QTranslator>
+#include <QMessageBox>
+#include <QLocale>
+#include "window.h"
+
+#include <QCleanlooksStyle>
+//
+int main(int argc, char ** argv)
+{
+	QApplication app( argc, argv );
+
+	QTranslator translator;
+	//printf("%s\n", QLocale::languageToString(QLocale::system().language()).toAscii().data());
+	if(! translator.load(QCoreApplication::applicationDirPath() + "/res/" + QLocale::languageToString(QLocale::system().language())))
+		if(! translator.load(QCoreApplication::applicationDirPath() + "/res/en.qm"))
+		{
+			QMessageBox::critical(NULL, "Error", "Error while loading language.\nCheck whether ./res/[(en)(de)].qm is available!");
+			return 1;
+		}
+
+	app.installTranslator(&translator);
+
+//	app.setWindowIcon(QIcon(QDir::toNativeSeparators(QCoreApplication::applicationDirPath() + "/res/isk1.png")));
+
+	MainWindow win;
+	win.show(); 
+
+	app.setStyle(new QCleanlooksStyle); // comment this if it won't compile
+	app.connect( &app, SIGNAL( lastWindowClosed() ), &app, SLOT( quit() ) );
+	return app.exec();
+}
---
+++ src/window.cpp
@@ -0,0 +1,52 @@
+
+/************************************************************************
+ * window.cpp								*
+ * Copyright (C) 2008  Psyjo						*
+ *									*
+ * This program is free software; you can redistribute it and/or modify	*
+ * it under the terms of the GNU General Public License as published by	*
+ * the Free Software Foundation; either version 3 of the License,	*
+ * or (at your option) any later version.				*
+ *									*
+ * This program is distributed in the hope that it will be useful, but	*
+ * WITHOUT ANY WARRANTY; without even the implied warranty of		*
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.			*
+ * See the GNU General Public License for more details.			*
+ *									*
+ * You should have received a copy of the GNU General Public License	*
+ * along with this program; if not, see <http://www.gnu.org/licenses/>. *
+ ************************************************************************/
+
+#include "window.h"
+
+MainWindow::MainWindow( QWidget * parent, Qt::WFlags f)
+        : QMainWindow(parent, f)
+{
+//	setWindowTitle(tr("appName"));
+	
+	mFile = menuBar()->addMenu(tr("&file"));
+	mFile->addAction(tr("exit"));
+	connect(mFile, SIGNAL(triggered(QAction*)), this, SLOT(handleFileAction(QAction*)));
+
+	about = menuBar()->addMenu(tr("&about"));
+	about->addAction("ESW");
+	about->addAction("Qt");
+	connect(about, SIGNAL(triggered(QAction*)), this, SLOT(handleAboutAction(QAction*)));
+
+	adjustSize();
+}
+
+void MainWindow::handleAboutAction(QAction* a)
+{
+	if (a->text() == "ESW") QMessageBox::about ( this, tr("about"), "ESW by Psyjo - (GPL)");
+	if (a->text() == "Qt") QMessageBox::aboutQt ( this, tr("about"));
+}
+
+void MainWindow::handleFileAction(QAction* a)
+{
+	if (a->text() == tr("exit")) close();
+}
+
+MainWindow::~MainWindow()
+{}
+
