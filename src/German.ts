38866f1e8827b9f3bf132f8aa59170308a7ac5bf
ThePsyjo <ThePsyjo@googlemail.com>
2008-12-16 14:36:10 +0000

    mod: about text

--- src/German.ts
+++ src/German.ts
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS><TS version="1.1" language="de_DE">
+<defaultcodec></defaultcodec>
 <context>
     <name>ApiInput</name>
     <message>
@@ -139,7 +140,7 @@ Konfiguration wird nicht geschrieben</translation>
     <message>
         <location filename="window.cpp" line="25"/>
         <source>appName</source>
-        <translation>EveSkillWatcher 0.1.1</translation>
+        <translation>EveSkillWatcher 0.1.2</translation>
     </message>
     <message>
         <location filename="window.cpp" line="109"/>
@@ -205,6 +206,35 @@ Konfiguration wird nicht geschrieben</translation>
         <location filename="window.cpp" line="186"/>
         <source>last update @ %1.</source>
         <translation>letztes Update @ %1.</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="102"/>
+        <source>%1
+ESW
+
+Copyright (C) 2008  Psyjo
+
+ This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ See the GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along with this program; if not, see http://www.gnu.org/licenses/.</source>
+        <translation>%1
+ESW
+
+Copyright (C) 2008  Psyjo
+
+Dieses Programm ist freie Software. Sie können es unter den Bedingungen der GNU General Public License, wie von der Free Software Foundation veröffentlicht, weitergeben und/oder modifizieren, entweder gemäß Version 3 der Lizenz oder (nach Ihrer Option) jeder späteren Version.
+
+Die Veröffentlichung dieses Programms erfolgt in der Hoffnung, daß es Ihnen von Nutzen sein wird, aber OHNE IRGENDEINE GARANTIE, sogar ohne die implizite Garantie der MARKTREIFE oder der VERWENDBARKEIT FÜR EINEN BESTIMMTEN ZWECK. Details finden Sie in der GNU General Public License.
+
+Sie sollten ein Exemplar der GNU General Public License zusammen mit diesem Programm erhalten haben. Falls nicht, siehe &lt;http://www.gnu.org/licenses/&gt;. </translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="102"/>
+        <source>appname</source>
+        <translation>EveSkillWatcher 0.1.2</translation>
     </message>
 </context>
 <context>
--- src/en.ts
+++ src/en.ts
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS><TS version="1.1" language="de_DE">
+<defaultcodec></defaultcodec>
 <context>
     <name>ApiInput</name>
     <message>
@@ -149,7 +150,7 @@ config will not be written</translation>
     <message>
         <location filename="window.cpp" line="25"/>
         <source>appName</source>
-        <translation>EveSkillWatcher 0.1.1</translation>
+        <translation>EveSkillWatcher 0.1.2</translation>
     </message>
     <message>
         <location filename="window.cpp" line="109"/>
@@ -215,6 +216,35 @@ config will not be written</translation>
         <location filename="window.cpp" line="186"/>
         <source>last update @ %1.</source>
         <translation>last update @ %1.</translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="102"/>
+        <source>%1
+ESW
+
+Copyright (C) 2008  Psyjo
+
+ This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ See the GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along with this program; if not, see http://www.gnu.org/licenses/.</source>
+        <translation>%1
+ESW
+
+Copyright (C) 2008  Psyjo
+
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program; if not, see &lt;http://www.gnu.org/licenses/&gt;. </translation>
+    </message>
+    <message>
+        <location filename="window.cpp" line="102"/>
+        <source>appname</source>
+        <translation>EveSkillWatcher 0.1.2</translation>
     </message>
 </context>
 <context>
